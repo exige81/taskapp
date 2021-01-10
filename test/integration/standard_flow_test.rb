1395eae394e429955786929dcd8ed53b5bee7e91
Justin Miller <miller.justin.r@gmail.com>
2021-01-09 20:52:14 -0800

    Basic login-logout

--- test/integration/standard_flow_test.rb
+++ test/integration/standard_flow_test.rb
@@ -7,5 +7,14 @@ class StandardFlowTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'a[href=?]', new_user_session_path
     assert_select 'a[href=?]', new_user_registration_path
+    assert_select 'a[href=?]', destroy_user_session_path, count: 0
+  end
+
+  test "Home page has logout when logged in" do
+    sign_in users(:normal_user)
+    get root_path
+    assert_select 'a[href=?]', new_user_session_path, count: 0
+    assert_select 'a[href=?]', new_user_registration_path, count: 0
+    assert_select 'a[href=?]', destroy_user_session_path
   end
 end
