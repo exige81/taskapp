b6df483f6b43c4f6ce83572b93abe4e3df4b58e1
Justin Miller <miller.justin.r@gmail.com>
2021-05-02 16:55:29 -0700

    Add option to skip parallel testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  unless ENV['SKIP_PARALLEL_TESTS'] then parallelize(workers: :number_of_processors) end
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
