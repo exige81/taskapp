4e104fe3b901381df30b64c565dcf5403073a7d0
Justin Miller <miller.justin.r@gmail.com>
2025-12-04 15:05:23 -0500

    Update stimulus reflex

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,5 +2,5 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
-  Capybara.default_max_wait_time = 300
+  Capybara.default_max_wait_time = 5
 end
